db95a21d044b9b07a7778cc7890a1a21f23ffbb4
Christian Flothmann <christian.flothmann@open.de>
2025-12-13 22:59:58 +0100

    do not use PHPUnit mock objects without configured expectations

--- Tests/FortySixElksTransportTest.php
+++ Tests/FortySixElksTransportTest.php
@@ -12,6 +12,7 @@
 namespace Symfony\Component\Notifier\Bridge\FortySixElks\Tests;
 
 use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
 use Symfony\Component\Notifier\Bridge\FortySixElks\FortySixElksTransport;
 use Symfony\Component\Notifier\Exception\TransportException;
 use Symfony\Component\Notifier\Message\ChatMessage;
@@ -20,7 +21,6 @@ use Symfony\Component\Notifier\Message\SmsMessage;
 use Symfony\Component\Notifier\Test\TransportTestCase;
 use Symfony\Component\Notifier\Tests\Transport\DummyMessage;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class FortySixElksTransportTest extends TransportTestCase
 {
@@ -47,10 +47,7 @@ class FortySixElksTransportTest extends TransportTestCase
 
     public function testSendSuccessfully()
     {
-        $response = $this->createMock(ResponseInterface::class);
-        $response->method('getStatusCode')->willReturn(200);
-        $response->method('getContent')->willReturn(file_get_contents(__DIR__.'/Fixtures/success-response.json'));
-        $client = new MockHttpClient($response);
+        $client = new MockHttpClient(new MockResponse(file_get_contents(__DIR__.'/Fixtures/success-response.json')));
         $transport = $this->createTransport($client);
         $sentMessage = $transport->send(new SmsMessage('+46701111111', 'Hello!'));
 
@@ -63,10 +60,7 @@ class FortySixElksTransportTest extends TransportTestCase
      */
     public function testExceptionIsThrownWhenSendFailed(int $statusCode, string $content, string $expectedExceptionMessage)
     {
-        $response = $this->createMock(ResponseInterface::class);
-        $response->method('getStatusCode')->willReturn($statusCode);
-        $response->method('getContent')->willReturn($content);
-        $client = new MockHttpClient($response);
+        $client = new MockHttpClient(new MockResponse($content, ['http_code' => $statusCode]));
         $transport = $this->createTransport($client);
 
         $this->expectException(TransportException::class);
